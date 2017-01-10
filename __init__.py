0669273fa00682a58dddf0449580a52a44ed3b75
Ankit <ankitml@gmail.com>
2017-01-10 15:54:02 +0530

    reshuffling of import code

--- __init__.py
+++ __init__.py
@@ -1,2 +1,1 @@
-from src import main
-
+from src import * 
---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+from .main import (each, map, reduce, reduce_right, find, select
+                   where, find_where, reject, every, some, contains,
+                   invoke, pluck, max, min, sort_by, group_by, index_by
+                   count_by, shuffle, sample, size, partition)
+
+__all__ = [
+        'each', 'map', 'reduce', 'reduce_right', 'find', 'select',
+        'where', 'find_where', 'reject', 'every', 'some', 'contains',
+        'invoke', 'pluck', 'max', 'min', 'sort_by', 'group_by', 
+        'index_by', 'count_by', 'shuffle', 'sample', 'size', 'partition'
+        ]
