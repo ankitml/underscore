ef32e365845feb42c60535c0bf209e77c99a95ee
Ankit <ankitml@gmail.com>
2017-01-15 14:38:03 +0530

    some more tests for flatten

--- tests/test_arrays.py
+++ tests/test_arrays.py
@@ -1,6 +1,7 @@
 import unittest
 from src import arrays as _
 
+
 class TestFlatten(unittest.TestCase):
 
     def test_flat_list(self):
@@ -12,6 +13,13 @@ class TestFlatten(unittest.TestCase):
         l = range(0, 10)
         flat = _.flatten(range(0,10))
         self.assertEqual(list(l), list(flat))
+
+    def test_single_nest_list(self):
+        l = [1, 2, 3, [4]]
+        flat = list(_.flatten(l))
+        self.assertEqual([1,2,3,4], flat)
+
+
 
 if __name__ == "__main__":
     unittest.main()
