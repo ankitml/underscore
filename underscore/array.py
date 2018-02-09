aef812f5ed9a0b168147960a41f81729cbc79848
Ankit <ankit.mittal@tophatmonocle.com>
2018-02-09 12:52:48 -0500

    added array.first

--- underscore/array.py
+++ underscore/array.py
@@ -1,3 +1,14 @@
+from iterrools import islice
+
+
+def first(iterable, n=1):
+    """
+    Returns the first element of an iterable. Passing n will return the first n elements of the iterable.
+    Does not modify the original iterator, copies it into a new generator. So if the iterable passed is
+    a generator it will not be consumed.
+    """
+    return islice(iterable, 0, n)
+
 def flatten(iterable, shallow=False):
     """
     Flattens an iterable. (generator, list etc)
@@ -12,4 +23,5 @@ def flatten(iterable, shallow=False):
                 yield element
     except TypeError:
         yield iterable
+
 
