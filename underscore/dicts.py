6c46b8ecb35664b16bd163592429448bcaacf7dd
Ankit <ankit.mittal@tophatmonocle.com>
2018-02-13 11:50:48 -0500

    two dict functions

---
+++ underscore/dicts.py
@@ -0,0 +1,13 @@
+def pick(d, *keys):
+    """
+    Return a copy of the object, filtered to only have values for the whitelisted keys (or array of valid keys). Alternatively accepts a predicate indicating which keys to pick.
+    """
+    pass
+
+
+def dict_map(d, func):
+    """
+    Like map, but for dictionaries. Transform the value of each property in turn.
+
+    """
+    pass
